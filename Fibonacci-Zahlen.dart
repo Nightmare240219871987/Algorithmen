4b39e0948cb0c5dbd3bcccccb191f138503a3e37
Marcus Schmidt <Marcusschmidt2402@hotmail.de>
2025-06-04 15:40:06 +0200

    Fibonacci-Zahlen implementiert

---
+++ Fibonacci-Zahlen.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+
+// fib=fn-1 + fn-2
+void main() {
+  List<int> fibNumbers = [];
+  int? input = int.tryParse(stdin.readLineSync()!);
+  int n1 = 1;
+  int n2 = 1;
+  int fib = 0;
+
+  if(input == null || input < 1) {
+    print("gib eine gültige und positive Zahl ein : ");
+    return;
+  }
+
+  if(input == 1){
+    print([1]);
+    return;
+  }
+
+  fibNumbers.addAll([1,1]);
+  for(int i = 0; i < (input!-2); i++){
+    fib = n1 + n2;
+    fibNumbers.add(fib);
+    n1 = n2; 
+    n2 = fib;       
+  }
+
+  print(fibNumbers);
+}
--- Test.dart
+++
@@ -1,7 +0,0 @@
-
-void main() {
-  var a = (int item) => {item = item + 1};
-  var b =  a(4);
-
-  print(b);
-}
